7bf623b8d46ad41699024bdeb63d6d69601183a6
Drew <andrewmurray@ou.edu>
2020-11-18 16:25:01 -0600

    commit

--- src/GUITest.java
+++ src/GUITest.java
@@ -1,9 +1,33 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class GUITest {
 
+	private static final int HEIGHT = 400;
+	private static final int WIDTH = 400;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// so far, all our progrmas were sequential
+		//they reach the END
+		JFrame frame = new JFrame("Dark Wave");
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FlowLayout layout = new FlowLayout();
+		frame.setLayout(layout);
+		
+		JButton b1 = new JButton("Man who stare");
+		JButton b2 = new JButton("at goats");
+		JButton b52 = new JButton("at code monkeys");
+		
+		frame.add(b1);
+		frame.add(b2);
+		frame.add(b52);
+		
+		frame.pack();
 	}
 
 }
